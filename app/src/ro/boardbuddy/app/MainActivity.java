37e0f72c9824392ffe72a8904d188fe20097bd9f
calindotgabriel <calindotgabriel@yahoo.com>
2014-08-31 15:26:32 +0300

    picasso and lva

--- app/src/ro/boardbuddy/app/MainActivity.java
+++ app/src/ro/boardbuddy/app/MainActivity.java
@@ -11,5 +11,7 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+
     }
 }
---
+++ commons/src/LongboardItem.java
@@ -0,0 +1,28 @@
+/**
+ * @author motan
+ * @date 8/31/14
+ */
+public class LongboardItem {
+
+    private String name;
+    private String description;
+
+    public LongboardItem() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
